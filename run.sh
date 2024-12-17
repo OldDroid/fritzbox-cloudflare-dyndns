0f55a69f6b62b24e0063620c53620a14f324d7ce
OldDroid <pohl199885@gmail.com>
2024-12-17 09:02:55 +0100

    hassio: CLOUDFLARE_API_KEY_FILE -> CLOUDFLARE_API_TOKEN_FILE

--- run.sh
+++ run.sh
@@ -11,9 +11,9 @@ export FRITZBOX_ENDPOINT_URL
 export FRITZBOX_ENDPOINT_INTERVAL
 export FRITZBOX_ENDPOINT_TIMEOUT
 
-echo "$(bashio::config 'cloudflare_api_key')" > /api_key
-chmod 600 /api_key
-export CLOUDFLARE_API_KEY_FILE="/api_key"
+echo "$(bashio::config 'cloudflare_api_token')" > /api_token
+chmod 600 /api_token
+export CLOUDFLARE_API_TOKEN_FILE="/api_token"
 
 export CLOUDFLARE_ZONES_IPV4
 export CLOUDFLARE_ZONES_IPV6
