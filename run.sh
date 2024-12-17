d8c9798aa91dbee267ca63ea08a298f93c521233
OldDroid <pohl199885@gmail.com>
2024-12-17 08:57:53 +0100

    hassio: dont expose api key in environment and introduce fritzbox endpoint interval option

--- run.sh
+++ run.sh
@@ -2,16 +2,19 @@
 
 # Read configuration from the add-on options
 FRITZBOX_ENDPOINT_URL=$(bashio::config 'fritzbox_endpoint_url')
+FRITZBOX_ENDPOINT_INTERVAL=$(bashio::config 'fritzbox_endpoint_interval')
 FRITZBOX_ENDPOINT_TIMEOUT=$(bashio::config 'fritzbox_endpoint_timeout')
-DYNDNS_SERVER_BIND=$(bashio::config 'dyndns_server_bind')
-CLOUDFLARE_API_KEY=$(bashio::config 'cloudflare_api_key')
 CLOUDFLARE_ZONES_IPV4=$(bashio::config 'cloudflare_zones_ipv4')
 CLOUDFLARE_ZONES_IPV6=$(bashio::config 'cloudflare_zones_ipv6')
 
 export FRITZBOX_ENDPOINT_URL
+export FRITZBOX_ENDPOINT_INTERVAL
 export FRITZBOX_ENDPOINT_TIMEOUT
-export DYNDNS_SERVER_BIND
-export CLOUDFLARE_API_KEY
+
+echo "$(bashio::config 'cloudflare_api_key')" > /api_key
+chmod 600 /api_key
+export CLOUDFLARE_API_KEY_FILE="/api_key"
+
 export CLOUDFLARE_ZONES_IPV4
 export CLOUDFLARE_ZONES_IPV6
 
